0daa2adeceb786352948f03070101666f29bd4c7
bernard-ng <ngandubernard@gmail.com>
2020-01-31 10:09:19 +0100

    feat: profile page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,57 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+    <div class="container">
+        <div class="row">
+            <div class="col-3 p-5">
+                <img class="rounded-circle"
+                     src="https://scontent-jnb1-1.cdninstagram.com/v/t51.2885-19/s150x150/81339546_2457344691181321_108807275547721728_n.jpg?_nc_ht=scontent-jnb1-1.cdninstagram.com&_nc_ohc=XxDnJJlDVHwAX-iqdSR&oh=e359f1ce233dce6f3753373e51372ef2&oe=5EB9300C"
+                     alt="profile">
+            </div>
+            <div class="col-9 pt-5">
+                <div><h1>Devscast</h1></div>
+                <div class="d-flex">
+                    <div class="pr-5"><strong>150</strong> posts</div>
+                    <div class="pr-5"><strong>2,4k</strong> followers</div>
+                    <div class="pr-5"><strong>212</strong> following</div>
                 </div>
+                <div class="pt-4 font-weight-bold">devs-cast.com</div>
+                <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam dolorum impedit incidunt
+                    quo
+                    sequi similique voluptatem. Culpa dicta, earum eius eligendi, ex facere harum iure iusto quia quod
+                    recusandae.
+                </div>
+                <div><a href="https://devs-cast.com" target="_blank">devs-cast.com</a></div>
+            </div>
+        </div>
+        <div class="row pt-5">
+            <div class="col-4">
+                <img alt="Image may contain: screen"
+                     class="w-100"
+                     decoding="auto"
+                     sizes="293px"
+                     src="http://localhost/photofills/500/500"
+                     style="object-fit: cover;"
+                >
+            </div>
+            <div class="col-4">
+                <img alt="Image may contain: screen"
+                     class="w-100"
+                     decoding="auto"
+                     sizes="293px"
+                     src="http://localhost/photofills/500/500"
+                     style="object-fit: cover;"
+                >
+            </div>
+            <div class="col-4">
+                <img alt="Image may contain: screen"
+                     class="w-100"
+                     decoding="auto"
+                     sizes="293px"
+                     src="http://localhost/photofills/500/500"
+                     style="object-fit: cover;"
+                >
             </div>
         </div>
     </div>
-</div>
 @endsection
